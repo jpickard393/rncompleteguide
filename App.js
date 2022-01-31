297a2f2ec1d1d87ce6a7e625f3ef5c393712eb6e
jpickard393 <jpickard@and.digital>
2022-01-31 17:59:27 +0000

    split into seperate components

--- App.js
+++ App.js
@@ -5,48 +5,27 @@
  * @format
  * @flow strict-local
  */
-
 import React, {useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  Button,
-  FlatList,
-} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = enteredText => {
-    setEnteredGoal(enteredText);
-  };
-  const addGoalHandler = () => {
+  const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
-      {id: Math.random().toString(), value: enteredGoal},
+      {id: Math.random().toString(), value: goalTitle},
     ]);
   };
   return (
     <View style={styles.screen}>
-      <View style={styles.input}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.inputContainer}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="Add" style={styles.addButton} onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={itemData => (
-          <View style={styles.listItem}>
-            <Text>{itemData.item.value}</Text>
-          </View>
-        )}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
       />
     </View>
   );
@@ -55,26 +34,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
-  },
-  inputContainer: {
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10,
-    width: '80%',
-  },
-  input: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  addButton: {
-    padding: 20,
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: 'silver',
-    borderColor: 'black',
-    borderWidth: 1,
   },
 });
